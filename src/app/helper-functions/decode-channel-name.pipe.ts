456674cc1ddfd21077dbc3cb2d5663f7f5a1c420
Vladi <v-ed@outlook.com>
2024-09-08 13:33:03 +0300

    added new channel names, updated CEO

--- src/app/helper-functions/decode-channel-name.pipe.ts
+++ src/app/helper-functions/decode-channel-name.pipe.ts
@@ -19,6 +19,17 @@ export function decodeChannelName(commandId?: string) {
     case 'FHR' : return 'Front Height Right';
     case 'RHL' : return 'Rear Height Left';
     case 'RHR' : return 'Rear Height Right';
+    case 'BDL' : return 'Back Dolby Left';
+    case 'CH' : return 'Center Height';
+    case 'FDL' : return 'Front Dolby Left';
+    case 'FWL' : return 'Front Wide Left';
+    case 'SBL' : return 'Surround Back Left';
+    case 'SDL' : return 'Surround Dolby Left';
+    case 'SHL' : return 'Surround Height Left';
+    case 'TFL' : return 'Top Front Left';
+    case 'TML' : return 'Top Middle Left';
+    case 'TRL' : return 'Top Rear Left';
+    case 'TS' : return 'Top Surround';
     case 'SW1' : return 'Subwoofer';
     case 'SW2' : return 'Subwoofer 2';
     case 'SWMIX' : return 'Subwoofer Mix';
