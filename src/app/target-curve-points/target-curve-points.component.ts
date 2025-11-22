5ea83cdac15650b77245d6b06f317527f0c8b8ca
Vladi <v-ed@outlook.com>
2025-11-23 01:41:13 +0200

    added cure points remove animation

--- src/app/target-curve-points/target-curve-points.component.ts
+++ src/app/target-curve-points/target-curve-points.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, output, ViewChild } from "@angular/core";
+import { ChangeDetectionStrategy, Component, effect, model, untracked, ViewChild } from "@angular/core";
 import { CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport } from "@angular/cdk/scrolling";
 import { FormsModule } from "@angular/forms";
 import { MatFormField, MatLabel } from "@angular/material/form-field";
@@ -6,36 +6,41 @@ import { PointsConverterPipe } from "./points-converter.pipe";
 import { MatInput } from "@angular/material/input";
 import { MatRipple } from "@angular/material/core";
 
-function copyArray(arr: string[] | undefined) {
-  if (arr?.length) return [...arr];
-  else return [];
-}
-
 @Component({
-    selector: 'app-target-curve-points',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    templateUrl: "./target-curve-points.component.html",
-    styleUrl: "./target-curve-points.component.scss",
-    imports: [
-        FormsModule,
-        MatFormField,
-        PointsConverterPipe,
-        MatInput,
-        MatLabel,
-        MatRipple,
-        CdkVirtualScrollViewport,
-        CdkVirtualForOf,
-        CdkFixedSizeVirtualScroll
-    ]
+  selector: 'app-target-curve-points',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./target-curve-points.component.html",
+  styleUrl: "./target-curve-points.component.scss",
+  imports: [
+    FormsModule,
+    MatFormField,
+    PointsConverterPipe,
+    MatInput,
+    MatLabel,
+    MatRipple,
+    CdkVirtualScrollViewport,
+    CdkVirtualForOf,
+    CdkFixedSizeVirtualScroll
+  ]
 })
 export class TargetCurvePointsComponent {
-  @Input({transform: copyArray, required: true})
-  curvePoints!: string[]; // always come as array
-  curvePointsChange = output<string[]>();
+  curvePoints = model.required<string[]>();
 
+  localPoints: string[] = [];
   showSaveBtn = false;
+
   @ViewChild(CdkVirtualScrollViewport)
   scrollViewport: CdkVirtualScrollViewport | undefined;
+
+  constructor() {
+    effect(() => {
+      const points = this.curvePoints();
+      untracked(() => {
+        this.localPoints = [...points];
+        this.showSaveBtn = false;
+      });
+    });
+  }
 
   changeItem(point: { Hz: string; vol: number }, index: number) {
     const hz = Number(point.Hz);
@@ -43,46 +48,46 @@ export class TargetCurvePointsComponent {
     if (hz > 20000) point.Hz = '20000';
 
     // point.Hz.padEnd(18, '0')
-    this.curvePoints[index] = '{' + point.Hz + ', ' + point.vol + '}';
+    this.localPoints[index] = '{' + point.Hz + ', ' + point.vol + '}';
     this.showSaveBtn = true;
   }
 
   addPoint() {
-    const lastPoint = this.curvePoints.at(-1);
+    const lastPoint = this.localPoints.at(-1);
     if (lastPoint) {
-      this.curvePoints = [...this.curvePoints, '{20000, 0}']; // need to create a new array for change detection
+      this.localPoints = [...this.localPoints, '{20000, 0}']; // need to create a new array for change detection
       this.scrollViewport?.scrollTo({ bottom: -25, behavior: 'smooth' });
 
       this.showSaveBtn = true;
     }
     else {
-      this.curvePoints = ['{, 0}'];
+      this.localPoints = ['{, 0}'];
     }
   }
 
   removePoint(index: number) {
-    this.curvePoints = this.curvePoints.filter((_, i) => i != index);
+    this.localPoints = this.localPoints.filter((_, i) => i != index);
     this.showSaveBtn = true;
   }
 
   save() {
     // Need to ensure points are sorted for export at the least, but it's nice
     // to keep them sorted in the UI as well when users are done editing
-    if (this.curvePoints.length) {
+    if (this.localPoints.length) {
       this.sortPoints();
-      const [firstFreq] = this.curvePoints[0].substring(1).split(',');
-      if (Number(firstFreq) > 20) this.curvePoints = ['{20, 0}', ...this.curvePoints];
+      const [firstFreq] = this.localPoints[0].substring(1).split(',');
+      if (Number(firstFreq) > 20) this.localPoints = ['{20, 0}', ...this.localPoints];
 
-      const [lastFreq] = this.curvePoints.at(-1)!.substring(1).split(',');
-      if (Number(lastFreq) < 20000) this.curvePoints.push('{20000, 0}');
+      const [lastFreq] = this.localPoints.at(-1)!.substring(1).split(',');
+      if (Number(lastFreq) < 20000) this.localPoints.push('{20000, 0}');
     }
-    this.curvePointsChange.emit(this.curvePoints);
+    this.curvePoints.set(this.localPoints);
     this.showSaveBtn = false;
   }
 
   copyPoints() {
     // navigator.clipboard.writeText(JSON.stringify(this.curvePoints));
-    sessionStorage.setItem('target curve points', JSON.stringify(this.curvePoints))
+    sessionStorage.setItem('target curve points', JSON.stringify(this.localPoints))
   }
 
   pastePoints() {
@@ -90,7 +95,7 @@ export class TargetCurvePointsComponent {
       const newPointsStr = sessionStorage.getItem('target curve points');
       if (newPointsStr) {
         const newPoints = JSON.parse(newPointsStr);
-        if (newPoints.length) this.curvePoints = newPoints;
+        if (newPoints.length) this.localPoints = newPoints;
         this.showSaveBtn = true;
       }
     }
@@ -100,25 +105,19 @@ export class TargetCurvePointsComponent {
   }
 
   private sortPoints() {
-    this.curvePoints.sort((a, b) => {
-      const [aFreq] = a.substring(1).split(',');
-      const [bFreq] = b.substring(1).split(',');
-      return Number(aFreq) - Number(bFreq);
+    this.localPoints.sort((a, b) => {
+      const getFreq = (str: string) => parseFloat(str.substring(1).split(',')[0]);
+      return getFreq(a) - getFreq(b);
     });
   }
 
   changeItemString(point: string, index: number) {
-
-    console.log('point', point, 'index', index);
-
     const regex = /\{([2-9]\d|[1-9]\d{2,3}|1\d{4}|20000)(\.\d+)?, -?(\d+(\.\d+)?)}/;
 
     if (regex.test(point)) {
-      console.log('matches');
-      this.curvePoints[index] = point;
+      this.localPoints[index] = point;
       this.showSaveBtn = true;
     } else {
-      // this.curvePoints[index] = "{20000, 0}";
       this.showSaveBtn = false;
     }
   }
