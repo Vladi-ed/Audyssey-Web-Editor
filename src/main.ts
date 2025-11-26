4fe8a2202f0a0cb12b0bbb3841b8f2b593f9f540
Vladi <v-ed@outlook.com>
2025-11-26 23:19:57 +0200

    added global error listeners to improve error handling in main bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideZoneChangeDetection } from "@angular/core";
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from "@angular/material/snack-bar";
+import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { provideHighcharts } from 'highcharts-angular';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { verticalPosition: 'top' } },
-    provideHighcharts(),
-  ]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideBrowserGlobalErrorListeners(),
+        { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { verticalPosition: 'top' } },
+        provideHighcharts(),
+    ]
 }).catch(err => console.error(err));
